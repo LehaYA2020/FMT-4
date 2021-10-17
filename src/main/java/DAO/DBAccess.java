c22489133be3ce696b2db6bd6777b616f5a16795
Alexey <lesha.Alex9.denisov@mail.ru>
2021-10-17 22:05:27 +0300

    Update DBAccess.java

--- src/main/java/DAO/DBAccess.java
+++ src/main/java/DAO/DBAccess.java
@@ -22,4 +22,24 @@ public class DbAccess {
     public String getPassword() {
         return password;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DbAccess dbAccess = (DbAccess) o;
+
+        if (url != null ? !url.equals(dbAccess.url) : dbAccess.url != null) return false;
+        if (user != null ? !user.equals(dbAccess.user) : dbAccess.user != null) return false;
+        return password != null ? password.equals(dbAccess.password) : dbAccess.password == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = url != null ? url.hashCode() : 0;
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
 }
